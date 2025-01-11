b07e3ecfb473bdad80b4ee8ad7bcaca07ce85dcd
KasaiRyoma <4521107r@hgu.jp>
2025-01-12 07:37:03 +0900

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -22,7 +22,7 @@ st.markdown(
         align-items: center;
         justify-content: center;
         z-index: 999; /* カメラ映像の上に配置 */
-        opacity: 0;
+        opacity: 1;
     }
 
     /* ホバー時のエフェクト */
