f3f557a7088e6f0ad7b4007b2fa92d9d7a7f9585
KasaiRyoma <4521107r@hgu.jp>
2025-01-12 08:06:51 +0900

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -17,8 +17,7 @@ st.markdown(
         border: none; /* 枠線を非表示 */
         cursor: pointer; /* ポインタ表示 */
         display: flex; /* 中央揃え用 */
-        align-items: center;
-        justify-content: center;
+        
         z-index: 999; /* カメラ映像の上に配置 */
     }
 
