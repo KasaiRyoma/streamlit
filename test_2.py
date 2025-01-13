c82273774de2c74f37381453dbaa726cbd101160
KasaiRyoma <4521107r@hgu.jp>
2025-01-13 15:24:46 +0900

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -272,19 +272,19 @@ def main():
         if sound_f == "オン" and mood == "明るい":
             #audio_placeholder.empty()
             time.sleep(0.5) #これがないと上手く再生されません
-            audio_placeholder.markdown(sound_a, unsafe_allow_html=True)
+            st.markdown(sound_a, unsafe_allow_html=True)
         elif sound_f == "オン" and mood == "暗い":
             #audio_placeholder.empty()
             time.sleep(0.5) 
-            audio_placeholder.markdown(sound_b, unsafe_allow_html=True)     
+            st.markdown(sound_b, unsafe_allow_html=True)     
         elif sound_f == "オン" and mood == "コメディ":
             #audio_placeholder.empty()
             time.sleep(0.5) 
-            audio_placeholder.markdown(sound_c, unsafe_allow_html=True)
+            st.markdown(sound_c, unsafe_allow_html=True)
         elif sound_f == "オン" and mood == "ホラー":
             #audio_placeholder.empty()
             time.sleep(0.5) 
-            audio_placeholder.markdown(sound_d, unsafe_allow_html=True)       
+            st.markdown(sound_d, unsafe_allow_html=True)       
 
 if __name__ == '__main__':
     main()
