3a79ce71730cb80299ed4870474b95da95e35f59
KasaiRyoma <4521107r@hgu.jp>
2025-01-14 05:28:10 +0900

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -50,7 +50,7 @@ def init_page():
         unsafe_allow_html=True
     )
 
-def invoke_llm(llm, text, image_base64):
+def invoke_llm(llm, text, image_base64=none):
     query = [
         (
             "user",
