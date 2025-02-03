4476349fd2627ed8606d2b458081ad2e0dc0be51
KasaiRyoma <4521107r@hgu.jp>
2025-02-04 06:39:38 +0900

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -172,7 +172,7 @@ def main():
                     f"文章の雰囲気は{mood}にしてください。"
                 ),
             }
-        if response1 == "不明":
+        if response1 == "不":
             response2 = "画像をうまく解析できませんでした。もう一度撮影してください。"   
         else:
             response2 = chatgpt(llm, query2_text[kana])    
